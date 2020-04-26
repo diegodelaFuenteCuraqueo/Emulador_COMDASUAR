400be1a9f3dca85551d1f6763d988a9fc4b82345
diegodelaFuenteCuraqueo <diego.delafuente@ug.uchile.cl>
2020-04-26 03:38:12 -0400

    v0.0.2 Compilador funcionando y visualizador de eventos, falta implementar linea de tiempo y reproductor MIDI

--- Web/js/text2COMDASUAR.js
+++ Web/js/text2COMDASUAR.js
@@ -4,3 +4,55 @@ function compilarTexto(){
 	cargarPartitura(textoIn);
 }
 
+
+
+$( document ).ready(function() {
+    console.log( "ready!" );
+
+});
+
+
+
+playActivado = false;
+
+var cursorDeTiempo = 0;
+window.setInterval(function(){
+	if(playActivado){
+		lineaDeTiempo(cursorDeTiempo++);
+		$("#btnPlay").html("Detener");
+	}else if(!playActivado){
+		cursorDeTiempo = 0;
+		clearInterval() ;
+		$("#btnPlay").html("Reproducir");
+
+	}
+}, 1);
+
+function play(){
+
+	if(!playActivado){		playActivado = true;
+	}else if(playActivado){ playActivado = false }
+}
+
+function lineaDeTiempo(MsIn){
+
+	//var eventos = 0;
+	var NotaVel = "";
+	var duractionTotal = seqOnsets[seqOnsets.length]+seqMs[seqMs.length];
+
+	if( seqOnsets.includes( MsIn) ){
+	//if(seqOnsets[ eventos ] == parseInt(MsIn) ){
+
+		index = seqOnsets.indexOf( MsIn);
+
+			NotaVel = seqMidinotes[index]+" "+64+" "+seqMs[index];
+
+			//eventos++;
+			$("#noteEventNote").text(seqMidinotes[index]);
+			$("#noteEventDur").text(seqMs[index]);
+			$("#noteEventVel").text(64);
+			console.log(NotaVel);
+
+	}
+
+}
