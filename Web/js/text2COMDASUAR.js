9365eaaa635fbbc4bb1464ad40176184dbd6bc39
diegodelaFuenteCuraqueo <diego.delafuente@ug.uchile.cl>
2020-04-26 21:03:13 -0400

    Itentando implementar reproductor MIDI

--- Web/js/text2COMDASUAR.js
+++ Web/js/text2COMDASUAR.js
@@ -54,3 +54,24 @@ function lineaDeTiempo(MsIn){
 	}
 
 }
+
+
+
+window.onload = function () {
+	MIDI.loadPlugin({
+		soundfontUrl: "./soundfont/",
+		instrument: "acoustic_grand_piano",
+		onprogress: function(state, progress) {
+			console.log(state, progress);
+		},
+		onsuccess: function() {
+			var delay = 0; // play one note every quarter second
+			var note = 50; // the MIDI note
+			var velocity = 127; // how hard the note hits
+			// play the note
+			MIDI.setVolume(0, 127);
+			MIDI.noteOn(0, note, velocity, delay);
+			MIDI.noteOff(0, note, delay + 0.75);
+		}
+	});
+};
